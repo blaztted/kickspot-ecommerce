22653b401a056c0a33dcf9b50a8a34c29433168d
blaztted <bbc.silva@gmail.com>
2024-03-21 14:46:44 +0000

    added functionality to visitUs button, now it opens a new window on google maps with the shop address

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
+const locationButton = document.getElementById("btn-map");
+
+// Add click event listener to the button
+locationButton.addEventListener("click", redirectToGoogleMaps);
+
+// Function to redirect to Google Maps with the specified address
+function redirectToGoogleMaps() {
+  const mapsUrl =
+    "https://www.google.com/maps/place/Lucky+Bastard/@38.8077894,-9.1842235,15z/data=!4m2!3m1!1s0x0:0x8602d9b455cd5e12?sa=X&ved=1t:2428&ictx=111";
+  // Open the Google Maps URL in a new tab
+  window.open(mapsUrl, "_blank");
+}
 
 if (bar) {
   bar.addEventListener("click", () => {
